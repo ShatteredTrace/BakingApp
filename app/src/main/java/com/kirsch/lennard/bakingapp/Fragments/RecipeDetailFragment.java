f5e51bad2823ed8874ae2bf6a0e140ee2b46ae07
ShatteredTrace <lk03@arcor.de>
2018-07-05 11:24:15 +0200

    Implemented Exoplayer

--- app/src/main/java/com/kirsch/lennard/bakingapp/Fragments/RecipeDetailFragment.java
+++ app/src/main/java/com/kirsch/lennard/bakingapp/Fragments/RecipeDetailFragment.java
@@ -1,22 +1,61 @@
 package com.kirsch.lennard.bakingapp.Fragments;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.media.session.MediaButtonReceiver;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.kirsch.lennard.bakingapp.Objects.Recipe;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+import com.kirsch.lennard.bakingapp.Activities.DetailActivity;
 import com.kirsch.lennard.bakingapp.Objects.RecipeStep;
 import com.kirsch.lennard.bakingapp.R;
 
-public class RecipeDetailFragment extends Fragment {
+import timber.log.Timber;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+public class RecipeDetailFragment extends Fragment implements ExoPlayer.EventListener{
     public static final String RECIPESTEP_ID = "recipeStepID";
+    private static final String TAG = RecipeDetailFragment.class.getSimpleName();
 
     private RecipeStep recipeStep;
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mPlayerView;
+    private MediaSessionCompat mMediaSession;
+    private PlaybackStateCompat.Builder mStateBuilder;
+    private NotificationManager mNotificationManager;
+    private Context mContext;
 
     //Mandatory constructor for instantiating the fragment
     public RecipeDetailFragment(){
@@ -34,12 +73,70 @@ public class RecipeDetailFragment extends Fragment {
             recipeStep = savedInstanceState.getParcelable(RECIPESTEP_ID);
         }
 
+        mContext = getContext();
         View rootView = inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+
+        mPlayerView = (SimpleExoPlayerView) rootView.findViewById(R.id.playerView);
+        mPlayerView.setDefaultArtwork(BitmapFactory.decodeResource(getResources(), R.drawable.placeholder));
 
         TextView recipeStepInstruction = rootView.findViewById(R.id.recipe_detail_instruction_textview);
         recipeStepInstruction.setText(recipeStep.description);
 
+        if(recipeStep.videoURL != null && !recipeStep.videoURL.equals("")){
+            Log.i("a", "start Player Init");
+            initializeMediaSession();
+            Log.i("a", "Passed init Media Session");
+            initializePlayer();
+            Log.i("a", "Passed init Player");
+        }
+
+
+
         return rootView;
+    }
+
+    private void initializeMediaSession(){
+        mMediaSession = new MediaSessionCompat(mContext, TAG);
+
+        mMediaSession.setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS | MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        mMediaSession.setMediaButtonReceiver(null);
+
+        mStateBuilder = new PlaybackStateCompat.Builder().setActions(
+                PlaybackStateCompat.ACTION_PLAY |
+                        PlaybackStateCompat.ACTION_PAUSE |
+                        PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
+                        PlaybackStateCompat.ACTION_PLAY_PAUSE);
+
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+        mMediaSession.setCallback(new MySessionCallback());
+
+        mMediaSession.setActive(true);
+    }
+
+    private void initializePlayer(){
+        if(mExoPlayer == null){
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(mContext, trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+
+            mExoPlayer.addListener(this);
+
+            String userAgent = Util.getUserAgent(mContext, "BakingApp");
+            Uri mediaUri = Uri.parse(recipeStep.getVideoURL());
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    mContext, userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private void releasePlayer(){
+        mNotificationManager.cancelAll();
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
     }
 
     @Override
@@ -53,5 +150,105 @@ public class RecipeDetailFragment extends Fragment {
 
     public void setRecipeStep(RecipeStep recipeStep) {
         this.recipeStep = recipeStep;
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if((playbackState == ExoPlayer.STATE_READY) && playWhenReady){
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PLAYING, mExoPlayer.getCurrentPosition(), 1f);
+        } else if ((playbackState == ExoPlayer.STATE_READY)){
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PAUSED, mExoPlayer.getCurrentPosition(), 1f);
+        }
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+        showNotification(mStateBuilder.build());
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+
+    }
+
+    private void showNotification(PlaybackStateCompat state){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+
+        int icon;
+        String play_pause;
+        if(state.getState() == PlaybackStateCompat.STATE_PLAYING){
+            icon = R.drawable.exo_controls_pause;
+            play_pause = "Pause";
+        } else {
+            icon = R.drawable.exo_controls_play;
+            play_pause = "Play";
+        }
+
+        NotificationCompat.Action playPauseAction = new NotificationCompat.Action(icon, play_pause,
+                MediaButtonReceiver.buildMediaButtonPendingIntent(mContext, PlaybackStateCompat.ACTION_PLAY_PAUSE));
+
+        NotificationCompat.Action restartAction = new NotificationCompat.Action(R.drawable.exo_controls_previous, "Restart",
+                MediaButtonReceiver.buildMediaButtonPendingIntent(mContext, PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS));
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(mContext, 0, new Intent(mContext, DetailActivity.class), 0);
+
+        builder.setContentTitle("Have Fun Baking")
+                .setContentText("Press play to continue the Video")
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.placeholder)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .addAction(restartAction)
+                .addAction(playPauseAction)
+                .setStyle(new android.support.v4.media.app.NotificationCompat.MediaStyle()
+                .setMediaSession(mMediaSession.getSessionToken())
+                .setShowActionsInCompactView(0,1));
+
+        mNotificationManager = (NotificationManager) mContext.getSystemService(NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, builder.build());
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mNotificationManager != null) {
+
+            releasePlayer();
+            mMediaSession.setActive(false);
+        }
+    }
+
+    private class MySessionCallback extends MediaSessionCompat.Callback{
+        @Override
+        public void onPlay() {
+            mExoPlayer.setPlayWhenReady(true);
+        }
+
+        @Override
+        public void onPause() {
+            mExoPlayer.setPlayWhenReady(false);
+        }
+
+        @Override
+        public void onSkipToPrevious() {
+            mExoPlayer.seekTo(0);
+        }
     }
 }
